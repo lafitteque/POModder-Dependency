72bfab90521ba026d52b6bcdf0c400951dd8d2c3
lafitteque <quentin.laf@gmail.com>
2025-03-20 14:21:34 +0100

    suite compatibilité

--- replacing_files/Tile.gd
+++ replacing_files/Tile.gd
@@ -22,23 +22,13 @@ func setType(chain: ModLoaderHookChain, type:String):
 	var data_mod = chain.reference_object.get_node("/root/ModLoader/POModder-Dependency").data_mod
 	var tile_mods = chain.reference_object.get_tree().get_nodes_in_group("tile-mods")
 	var main_node : Node = chain.reference_object
-	
-	chain.execute_next([type])
-	for mod in tile_mods :
-		if mod.set_meta_destructable(main_node,type) :
-			set_meta("destructable", true)
-			
-	if type == "chaos":
-		print("debug")
-	if ! (type in data_mod.TILE_ID_TO_STRING_MAP.values()):
-		return
 		
 	var baseHealth:float = Data.of("map.tileBaseHealth")
 	
 	for mod in tile_mods :
 		baseHealth = mod.setType(main_node,type, baseHealth)
 		if mod.set_meta_destructable(main_node,type) :
-			set_meta("destructable", true)
+			main_node.set_meta("destructable", true)
 			
 	var healthMultiplier = main_node.hardnessMultiplier
 	healthMultiplier *= (pow(Data.of("map.tileHealthMultiplierPerLayer"), main_node.layer))
@@ -46,6 +36,8 @@ func setType(chain: ModLoaderHookChain, type:String):
 	main_node.max_health = max(1, round(healthMultiplier * baseHealth))
 	main_node.health = main_node.max_health 
 
+	chain.execute_next([type])
+	
 	
 func hit(chain: ModLoaderHookChain, dir:Vector2, dmg:float):
 	var tile_mods = chain.reference_object.get_tree().get_nodes_in_group("tile-mods")
